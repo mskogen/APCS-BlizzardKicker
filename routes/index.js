fffa1f6860b7a4c4877b98c6f8cd682523bcdccb
BillFlan <asherfarr99@gmail.com>
2018-10-15 17:12:12 -0600

    Mongodb support

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator/check');
+
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', (req, res) => {
 	res.render('form', {title:'Registration Form'});
@@ -18,7 +21,10 @@ router.post('/', [
 		const errors = validationResult(req);
 
 		if (errors.isEmpty()){
-			res.send('Thank you for your registration!');
+			const registration = new Registration(req.body);
+			registration.save()
+				.then(() => { res.send('Thank you for your registration!'); })
+				.catch(() => { res.send('Sorry something went wrong.');})
 		}else{
 			res.render('form', {
 				title: 'Registration form',
--- start.js
+++ start.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+require('./models/Registration');
 const app = require('./app');
+
+mongoose.connect(process.env.DATABASE, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
