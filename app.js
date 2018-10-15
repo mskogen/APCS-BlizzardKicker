75344a39e9a45e832d28ebc35e606fdc92860d35
BillFlan <asherfarr99@gmail.com>
2018-10-15 14:36:24 -0600

    Moving through a tutorial

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = require('./routes/index');
+const path = require('path');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.render('form', {title:'Registration Form'});
 });
 
 module.exports = router;
