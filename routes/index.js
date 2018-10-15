51a0a54085e18aea8511e5274507356792d84fbc
BillFlan <asherfarr99@gmail.com>
2018-10-15 15:16:14 -0600

    Installed express-validator

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
+const { body, validationResult } = require('express-validator/check');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('form', {title:'Registration Form'});
+	res.render('form', {title:'Registration Form'});
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.render('form', { title: 'Registration form' });
+router.post('/', [
+	body('name')
+    	.isLength({ min: 1 })
+    	.withMessage('Please enter a name'),
+    body('email')
+    	.isLength({ min: 1 })
+    	.withMessage('Please enter an email'),
+    ],
+
+	(req, res) => {
+		console.log(req.body);
+		res.render('form', { title: 'Registration form' });
 });
 
 module.exports = router;
