f748ba9220ba32a1ee63a065ca7832bf052cfcae
Kamiar Coffey <kamiar.coffey@gmail.com>
2018-11-03 15:41:12 -0600

    Updated API urls

--- models/DataCacheAPI.js
+++ models/DataCacheAPI.js
@@ -1,4 +1,5 @@
-// Kamiar
+// Author: Kamiar Coffey
+// Purpose: backend structure for storing API data in mongoDB
 
 // Weather unlocked does not allow US ski resort data on free version
 // username: APCS pw: blizzardkicker email: kamiar.junk@gmail.com
@@ -11,16 +12,15 @@
 // api.weatherunlocked.com/api/snowreport/999001?app_id={854428ed}&app_key={ea41306d613ed7bbcfcbb8ece0c62b06}
 
 
-
 const mongoose = require('mongoose');
 
-const APIdataSchema = new mongoose.Schema({
+const dataCacheSchema = new mongoose.Schema({
   snowDepth: {
-    type: Float,
+    type: Number,
   },
-  currentTempF: {
-    type: Float,
+  currentTemp: { // Farenheit
+    type: Number,
   },
 });
 
-module.exports = mongoose.model('DataCacheAPI', APIdataSchema);
+module.exports = mongoose.model('DataCacheAPI', dataCacheSchema);
