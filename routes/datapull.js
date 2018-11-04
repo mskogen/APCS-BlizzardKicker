d8c7e826558baca61749949154fe6b86277113a4
Kamiar Coffey <kamiar.coffey@gmail.com>
2018-11-04 13:05:10 -0700

    update node_modules include XMLHttpRequest

--- routes/datapull.js
+++ routes/datapull.js
@@ -12,40 +12,38 @@
 // api/snowreport/{resort_id}?app_id={854428ed}&app_key={ea41306d613ed7bbcfcbb8ece0c62b06}
 // api.weatherunlocked.com/api/snowreport/999001?app_id={854428ed}&app_key={ea41306d613ed7bbcfcbb8ece0c62b06}
 
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+var use = require('../models/DataCacheAPI.js')
+const DataCacheAPI = mongoose.model('DataCacheAPI');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const router = express.Router();
-//
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-//
-// const DataCacheAPI = mongoose.model('DataCacheAPI');
-//
-// // router.get('/', (req, res) => {
-// //   res.render('updateAPI');
-// // });
-//
-// // router.post('/updateAPI',
-// //   (req, res) => {
-// //   }
-// // );
-//   const callAPI = new XMLHttpRequest();
-//   const url='https://api.weatherunlocked.com/api/snowreport/333019?app_id=854428ed&app_key=b75b1bb6575f88dbf10b279301b0d7e4';
-//   callAPI.open("GET", url);
-//   callAPI.send();
-//   callAPI.onreadystatechange=function() {
-//     if(this.readyState==4 && this.status==200) {
-//       var data_object = JSON.parse(callAPI.responseText)
-//
-//       var resortName = new DataCacheAPI( {resortname: data_object.resortname} );
-//       resortName.save()
-//
-//       const newsnow_in = new DataCacheAPI( {newsnow_in: data_object.newsnow_in} );
-//       newsnow_in.save()
-//
-//       res.send('API pull successful');
+// router.get('/', (req, res) => {
+//   res.render('datapull');
+// });
+
+// router.post('/updateAPI',
+//   (req, res) => {
 //   }
-// }
-//
-// module.exports = router;
+// );
+  const callAPI = new XMLHttpRequest();
+  const url='https://api.weatherunlocked.com/api/snowreport/333019?app_id=854428ed&app_key=b75b1bb6575f88dbf10b279301b0d7e4';
+  callAPI.open("GET", url);
+  callAPI.send();
+  callAPI.onreadystatechange=function() {
+    if(this.readyState==4 && this.status==200) {
+      var data_object = JSON.parse(callAPI.responseText)
+
+      var resortName = new DataCacheAPI( {resortname: data_object.resortname} );
+      resortName.save()
+
+      const newsnow_in = new DataCacheAPI( {newsnow_in: data_object.newsnow_in} );
+      newsnow_in.save()
+
+      //res.send('API pull successful');
+  }
+}
+
+module.exports = router;
