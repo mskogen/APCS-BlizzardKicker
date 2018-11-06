c964f4fcbbba8a11d24bdf269abc39432abedf5e
Matthew Skogen <matt.skogen@aol.com>
2018-11-05 18:33:47 -0700

    Committing changes necesary for the login as a popup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const index = require('./routes/index');
-const login = require('./routes/login');
+//const login = require('./routes/login');
 const cave = require('./routes/cave');
 const reg = require('./routes/register');
 const prefs = require('./routes/userPrefs');
@@ -19,7 +19,7 @@ app.use(express.static('styles'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/login', login);
+//app.use('/login', login);
 app.use('/', index);
 app.use('/cave', cave);
 app.use('/register', reg);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose'); //mongodb databases
-
+const { body, validationResult } = require('express-validator/check'); //checks inputs for validity
 const router = express.Router();
+const Registration = mongoose.model('Registration'); //uses registration database
 
 router.get('/', (req, res) => {
 	res.render('index');
 });
+
+//handles post requests
+router.post('/', [ //allow input into form
+	body('email')
+		.isLength({ min: 1 })
+		.withMessage('Please enter a name'),
+	body('pass')
+		.isLength({ min: 1 })
+		.withMessage('Please enter a password'),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (errors.isEmpty()){
+			const registration = new Registration(req.body);
+			registration.save()
+				.then(() => { res.send('Thank you for your registration!'); })
+				.catch(() => { res.send('Sorry something went wrong.');})
+		}else{
+			res.render('login', {
+				errors: errors.array(),
+				data: req.body,
+				});
+		}
+});
+
 module.exports = router;
