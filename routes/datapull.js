136dbee494d3d815c1765fa4363f2a3312a3e60d
Kamiar Coffey <kamiar.coffey@gmail.com>
2018-11-14 12:11:20 -0700

    saving mid-work

--- routes/datapull.js
+++ routes/datapull.js
@@ -12,6 +12,12 @@
 // api/snowreport/{resort_id}?app_id={854428ed}&app_key={ea41306d613ed7bbcfcbb8ece0c62b06}
 // api.weatherunlocked.com/api/snowreport/999001?app_id={854428ed}&app_key={ea41306d613ed7bbcfcbb8ece0c62b06}
 
+// Collections
+// MongoDB stores documents in collections. Collections are analogous to tables in relational databases.
+
+// http://clientservice.onthesnow.com/externalservice/resort/[resort_id]/snowreport?token=[token]
+// where [resort_id] indicates the particular resort requested and [token] is the authorization token unique to your account.
+
 const express = require('express'); // use express routing
 const mongoose = require('mongoose'); // use mongo database
 const router = express.Router();
@@ -29,11 +35,26 @@ router.get('/', function (req, res) { // upp data upon get request
       if(this.readyState==4 && this.status==200) {
         var data_object = JSON.parse(callAPI.responseText) // turn the JSON response into an object
 
-        var resort = new DataCacheAPI( {resortname: data_object.resortname, newsnow_in: data_object.newsnow_in} ); // put the data into the mongoDB object
+        // Method works via Mongodb unique key resolution
+        // manually make the uniqye key _id the resort ID, rather than mongoDB generated code
+        // adding a 'new' resort with the same key overwrites the previous object
+        var current_time = new Date()
+        var resort = new DataCacheAPI( {_id: data_object.resortid,
+                                        time_stamp: current_time,
+                                        resort_name: data_object.resortname,
+                                        newsnow_in: data_object.newsnow_in,
+                                        percent_terrain_open: data_object.ADDME,
+                                        current_temperature: data_object.ADDME,
+                                        snow_conition: data_object.ADDME,
+                                        num_expert_runs: data_object.ADDME,
+                                        num_intermediate_runs: data_object.ADDME,
+                                        num_beginner_runs: data_object.ADDME,
+                                        new_snow_24h: data_object.ADDME,
+                                        new_snow_48h: data_object.ADDME,
+                                        new_snow_72h: data_object.ADDME,
+                                        new_snow_24h: data_object.ADDME,
+                                        snow_conition: data_object.ADDME,})
         resort.save()
-
-        // const newsnow_in = new DataCacheAPI(  );
-        // newsnow_in.save()
       }
     }
     res.render('status_API');
--- routes/userPrefs.js
+++ routes/userPrefs.js
@@ -1,3 +1,29 @@
+// Author: Kamiar Coffey
+// script to determine if a new API pull is needed
+
+// Takes as input - currentl user prefs - time of last pull from API - use mongoDB dataCache
+
+// db.DataCacheAPI.find(query, projection)
+
+// var record = db.data.findOne();
+// var localNow = new Date( record.date.getTime() -  ( record.offset * 60000 ) );
+
+
+// MongoClient mongo = new MongoClient( "localhost" , 27017 );
+// DB db = mongo.getDB(dbName);
+// DBCollection collection = db.getCollection(collectionName);
+//
+// BasicDBObject whereQuery = new BasicDBObject();
+// whereQuery.put("movie_id", id);
+//
+// DBObject document = collection.findOne(whereQuery);
+// BasicDBList list = (BasicDBList) document.get("genre");
+//
+// List<String> res = new ArrayList<String>();
+//
+// for(Object el: list) {
+//      res.add((String) el);
+// }
 
 // Fill in areas once dataypes from API are concrete -Kaimar
 
@@ -11,4 +37,3 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
-
