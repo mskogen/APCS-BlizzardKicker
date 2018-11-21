5ad4421dd83824f615f2647689c0f1672135364b
BillFlan <asherfarr99@gmail.com>
2018-11-21 01:43:30 -0700

    Added more skiData that we can pull

--- Scraping/skiDataScraper.js
+++ Scraping/skiDataScraper.js
@@ -5,7 +5,21 @@ const $ = require('cheerio');
 const skiData = function(url) {
 	return rp(url)
 	.then(function(html) {
-		return {
+		//create a list of dates for snowfall
+		var dateList= [];
+		$('._report_content .time span', html).each(function(i, elem){
+			dateList.push($(this).text());
+		});
+
+		//create a list of snowdepths
+		var snowFallList=[];
+		$('._report_content .predicted_snowfall .bluePill', html).each(function(i, elem){
+			snowFallList.push($(this).text());
+		});
+
+		var skiInfo= {
+			resort: $('title', html).text().split(" ",$('title', html).text().split(" ").length-4).join(" "),
+			
 			condition:{
 				upper: {
 					snow: $('.elevation.upper > .bluePill', html).text(),
@@ -22,14 +36,24 @@ const skiData = function(url) {
 			},
 			snowfall: {
 				today: $('._report_content .today .bluePill', html).text(),
-				/*
-				historical: 
-					{
-						$('._report_content .time span', html)[0].text(): $('._report_content .time .bluePill', html)[0].text(),
-					}
-				*/
+				
+				historical: {
+
+				},
+				predicted: {
+				},
 			},
-	  };
+	  	};
+	  	//loop through dates and add them to the historical data
+		for (var i = 0; i < dateList.length-3; i++) {
+			skiInfo.snowfall.historical[dateList[i]] =snowFallList[i];
+		}
+		//loop through dates and add the ones in the future
+		for (var i = dateList.length-3; i < dateList.length; i++) {
+			skiInfo.snowfall.predicted[dateList[i]] =snowFallList[i];
+		}
+		
+		return skiInfo;
 	})
 	.catch(function(err) {
 	  //handle error
