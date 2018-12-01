55d5087158234d3c924c5c8b980266c1998250a3
Kamiar Coffey <kamiar.coffey@gmail.com>
2018-12-01 10:55:59 -0700

    dependencies

--- routes/cave.js
+++ routes/cave.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose'); //mongodb databases
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.render('cave');
-});
-module.exports = router;
+// router.get('/', (req, res) => {
+// 	res.render('cave');
+// });
+
+var USERLIBRARY = USERLIBRARY || (function(){
+    var _args = {}; // private
+
+    return {
+        init : function(Args) {
+            _args = Args;
+            // some other initialising
+        },
+        helloWorld : function() {
+            alert('Hello World! -' + _args[0]);
+        }
+    };
+}());
+
+const domContainer = document.querySelector('#button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+// module.exports = router;
