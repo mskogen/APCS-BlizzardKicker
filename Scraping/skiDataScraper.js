9b918265b3a396a9a7bcf772e3aa52c2669a6010
BillFlan <asherfarr99@gmail.com>
2018-12-03 13:27:23 -0700

    Added comments and touched up index to look prettier

--- Scraping/skiDataScraper.js
+++ Scraping/skiDataScraper.js
@@ -7,14 +7,17 @@ const skiData = function(url) {
 	.then(function(html) {
 		//create a list of dates for snowfall
 		var dateList= [];
+		//get every date and push it to the list
 		$('._report_content .time span', html).each(function(i, elem){
 			dateList.push([$(elem).text()]);
 		});
+		//get every snowfall and add it to it's date pair
 		$('._report_content .predicted_snowfall .bluePill', html).each(function(i, elem){
 			dateList[i].push($(elem).text());
 		});
-
+		//skiInfo is the object containing all the ski info
 		var skiInfo= {
+			//get resort using all the words in the title except the last 4
 			resort: $('title', html).text().split(" ",$('title', html).text().split(" ").length-4).join(" "),
 
 			condition:{
@@ -31,17 +34,17 @@ const skiData = function(url) {
 					condition: $('.elevation.lower strong', html).text(),
 				},
 			},
+			//historical and predicted are added using dateList
 			snowfall: {
 				today: $('._report_content .today .bluePill', html).text(),
 				
 				historical: {
-
 				},
 				predicted: {
 				},
 			},
 	  	};
-
+	  	//seperates the last 3 from the group because they're historical
 		skiInfo.snowfall.historical=dateList.slice(0,dateList.length-3);
 		skiInfo.snowfall.predicted=dateList.slice(-3);
 		return skiInfo;
