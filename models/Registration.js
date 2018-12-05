0b8c9b7308fe509fb39bad164113070b09cf0eb0
BillFlan <asherfarr99@gmail.com>
2018-12-04 21:55:20 -0700

    Password validation

--- models/Registration.js
+++ models/Registration.js
@@ -12,6 +12,11 @@ var UserSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+  username:{
+    type: String,
+    unique: true,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
--- routes/register.js
+++ routes/register.js
@@ -2,33 +2,36 @@ const express = require('express');
 const mongoose = require('mongoose'); //mongodb databases
 const router = express.Router();
 const User = require('../models/Registration');
+const url = require('url');
 
 const { body, validationResult } = require('express-validator/check'); //checks inputs for validity
 const bodyParser = require('body-parser');
 
 router.get('/', (req, res) => {
-  res.render('register');
+  res.render('register', {error:req.query.err});
 });
 
 router.post('/', function (req, res, next) {
   if (req.body.email && req.body.password) { // both filled, create new user
     var userData = {
       email: req.body.email,
+      username: req.body.username,
       password: req.body.password,
     }
-      console.log('user data constructed!');
+      //console.log('user data constructed!');
 
 
     User.create(userData, function (error, user) {
       if (error) {
         res.redirect(url.format({
-          pathname: '/login',
+          pathname: '/register',
           query:{
-            "err":error.message
+            //"err":"User " + req.body.email +" is already registered."
+            "err": error.message
           }
         }));
       } else {
-          console.log('user created!');
+          console.log('New User: ' +user.email+' created!');
           req.session.userId = user.email;
           res.redirect('/cave');
       }
