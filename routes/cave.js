38f8c1fb73c8d716a2424346bfb056afb2a06517
Kamiar Coffey <kamiar.coffey@gmail.com>
2018-12-06 11:23:53 -0700

    logout now works

--- routes/cave.js
+++ routes/cave.js
@@ -10,15 +10,9 @@ var currentUser = 'Not found';
 
 const router = express.Router();
 
-
-
-// function logOut() {
-// req.session.destroy();
-// return res.redirect('/');
-// }
+var data = {currentUser: currentUser, bestChoice:'Make sure you have saved some resorts to your profile!'};
 
 router.get('/', (req, res) => {
-	var data = {currentUser: currentUser, bestChoice:'Looks like its barren out there'};
 	skiData("https://www.onthesnow.com/colorado/loveland/skireport.html").then((skiInfo) => {
 		var skiData ={
 			time_stamp: new Date(),
@@ -44,19 +38,53 @@ router.get('/', (req, res) => {
 	currentUser = req.session.userId;
 	data.currentUser = currentUser;
 	// console.log('from cave.js', currentUser);
+	res.render('cave', {data});
+});
 
+// router.get('/cave/alg', (req, res) => {
+// var userResorts = [];
+// var atLeastSomePrefs = false;
+// 	User.findOne({email: currentUser}).exec(function (err, user) {
+// 	// console.log(user.resort_id_list.length);
+// 		for (var i=0; i<user.resort_id_list.length; i++) {
+// 			var resortObject = {name:"", condition:"", snowfall: 0};
+// 			resortObject.name = user.resort_id_list[i];
+// 			atLeastSomePrefs = true;
+// 			// userResorts.push(resortObject.name);
+// 			// if (!atLeastSomePrefs) {
+// 			// 	res.render('cave', {data});
+// 			// }
+// 			Resort.findOne({resort_name: resortObject.name}).exec(function(err,resort) {
+// 				if (resort.resort_name == resortObject.name) {
+// 					resortObject.condition = resort.snow_condition;
+// 					resortObject.snowfall = resort.newsnow_in;
+// 					userResorts.push(resortObject);
+// 				}
+// 			});
+// 		}
+// 	});
+// 	setTimeout(function(){
+// 		// return userResorts object with highest resortObject.newsnow_in;
+// 		userResorts.sort(function(a, b){return b.snowfall - a.snowfall})
+// 		// console.log(userResorts);
+// 		if (userResorts.length) {
+// 			var bestChoice = userResorts[0].name;
+// 			data.bestChoice = bestChoice;
+// 			console.log('from algo', data.bestChoice);
+// 		}
+// 		// res.render('cave', bestChoice);
+// 	},1000);
+// res.render('cave', {data});
+
+router.get('/cave/alg', function (req, res) {
+	currentUser = req.session.userId;
 	var userResorts = [];
-	var atLeastSomePrefs = false;
 		User.findOne({email: currentUser}).exec(function (err, user) {
 		// console.log(user.resort_id_list.length);
 			for (var i=0; i<user.resort_id_list.length; i++) {
 				var resortObject = {name:"", condition:"", snowfall: 0};
 				resortObject.name = user.resort_id_list[i];
-				atLeastSomePrefs = true;
 				// userResorts.push(resortObject.name);
-				// if (!atLeastSomePrefs) {
-				// 	res.render('cave', {data});
-				// }
 				Resort.findOne({resort_name: resortObject.name}).exec(function(err,resort) {
 					if (resort.resort_name == resortObject.name) {
 						resortObject.condition = resort.snow_condition;
@@ -68,55 +96,27 @@ router.get('/', (req, res) => {
 		});
 		setTimeout(function(){
 			// return userResorts object with highest resortObject.newsnow_in;
-			userResorts.sort(function(a, b){return b.snowfall - a.snowfall})
-			// console.log(userResorts);
+			userResorts.sort(function(a, b){return b.snowfall - a.snowfall});
 			if (userResorts.length) {
+				console.log(userResorts);
 				var bestChoice = userResorts[0].name;
 				data.bestChoice = bestChoice;
 				console.log('from algo', data.bestChoice);
 			}
-			// res.render('cave', bestChoice);
-		},1000);
-	res.render('cave', {data});
+			res.render('cave', {data});
+		}, 1000);
 });
-
-// router.post('/cave/alg', (req, res) => {
-// 	currentUser = req.session.userId;
-// 	var userResorts = [];
-// 		User.findOne({email: currentUser}).exec(function (err, user) {
-// 		// console.log(user.resort_id_list.length);
-// 			for (var i=0; i<user.resort_id_list.length; i++) {
-// 				var resortObject = {name:"", condition:"", snowfall: 0};
-// 				resortObject.name = user.resort_id_list[i];
-// 				// userResorts.push(resortObject.name);
-// 				Resort.findOne({resort_name: resortObject.name}).exec(function(err,resort) {
-// 					if (resort.resort_name == resortObject.name) {
-// 						resortObject.condition = resort.snow_condition;
-// 						resortObject.snowfall = resort.newsnow_in;
-// 						userResorts.push(resortObject);
-// 					}
-// 				});
-// 			}
-// 		});
-// 		setTimeout(function(){
-// 			// return userResorts object with highest resortObject.newsnow_in;
-// 			userResorts.sort(function(a, b){return b.snowfall - a.snowfall})
-// 			console.log(userResorts);
-// 			var bestChoice = userResorts[0].name;
-// 			console.log('from algo', bestChoice);
-// 			res.render('cave', bestChoice);
-// 		},1000);
-// });
 
 // GET for logout logout
 router.get('/cave/logout', function (req, res, next) {
-  if (req.session) {
+	console.log('logging out current user');
+	if (req.session) {
     // delete session object
     req.session.destroy(function (err) {
       if (err) {
         return next(err);
       } else {
-        return res.redirect('index');
+        return res.render('index');
       }
     });
   }
